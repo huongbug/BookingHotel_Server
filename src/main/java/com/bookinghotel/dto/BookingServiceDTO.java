28b83a59059ac61ce55292ef92a0ef735d7109bf
huongbug <huongbug@gmail.com>
2023-04-03 17:02:51 +0700

    [week-3] add validation booking service dto

--- src/main/java/com/bookinghotel/dto/BookingServiceDTO.java
+++ src/main/java/com/bookinghotel/dto/BookingServiceDTO.java
@@ -1,9 +1,12 @@
 package com.bookinghotel.dto;
 
+import com.bookinghotel.constant.ErrorMessage;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,8 +14,10 @@ import lombok.Setter;
 @Getter
 public class BookingServiceDTO {
 
+  @NotNull(message = ErrorMessage.INVALID_SOME_THING_FIELD_IS_REQUIRED)
   private Long serviceId;
 
+  @NotNull(message = ErrorMessage.INVALID_SOME_THING_FIELD_IS_REQUIRED)
   private Integer amount;
 
 }
