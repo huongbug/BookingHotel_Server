c0c11f644adec64870eec37b6a1e857d05647a95
huongbug <huongbug@gmail.com>
2023-04-06 22:56:12 +0700

    [week-4] update api booking room/service

--- src/main/java/com/bookinghotel/controller/BookingController.java
+++ src/main/java/com/bookinghotel/controller/BookingController.java
@@ -2,9 +2,11 @@ package com.bookinghotel.controller;
 
 import com.bookinghotel.base.RestApiV1;
 import com.bookinghotel.base.VsResponseUtil;
+import com.bookinghotel.constant.BookingStatus;
 import com.bookinghotel.constant.RoleConstant;
 import com.bookinghotel.constant.UrlConstant;
 import com.bookinghotel.dto.BookingCreateDTO;
+import com.bookinghotel.dto.BookingFilterDTO;
 import com.bookinghotel.dto.common.DateFilterDTO;
 import com.bookinghotel.dto.pagination.PaginationSortRequestDTO;
 import com.bookinghotel.security.AuthorizationInfo;
@@ -14,13 +16,11 @@ import com.bookinghotel.service.BookingService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -30,6 +30,7 @@ public class BookingController {
 
   private final BookingService bookingService;
 
+  @Tags({@Tag(name = "booking-controller-admin"), @Tag(name = "booking-controller")})
   @Operation(summary = "API get booking by id")
   @AuthorizationInfo(role = { RoleConstant.ADMIN, RoleConstant.USER })
   @GetMapping(UrlConstant.Booking.GET_BOOKING)
@@ -41,7 +42,8 @@ public class BookingController {
   @AuthorizationInfo(role = { RoleConstant.USER })
   @GetMapping(UrlConstant.Booking.GET_BOOKINGS_FOR_USER)
   public ResponseEntity<?> getBookingsForUser(@Valid @ParameterObject PaginationSortRequestDTO pagination,
-                                              @Parameter(name = "principal", hidden = true) @CurrentUserLogin UserPrincipal principal) {
+                                              @Parameter(name = "principal", hidden = true)
+                                              @CurrentUserLogin UserPrincipal principal) {
     return VsResponseUtil.ok(bookingService.getBookingsForUser(pagination, principal));
   }
 
@@ -50,15 +52,16 @@ public class BookingController {
   @AuthorizationInfo(role = { RoleConstant.ADMIN })
   @GetMapping(UrlConstant.Booking.GET_BOOKINGS_FOR_ADMIN)
   public ResponseEntity<?> getBookingsForAdmin(@Valid @ParameterObject PaginationSortRequestDTO pagination,
-                                               @ParameterObject DateFilterDTO dateFilterDTO) {
-    return VsResponseUtil.ok(bookingService.getBookingsForAdmin(pagination, dateFilterDTO));
+                                               @ParameterObject BookingFilterDTO bookingFilterDTO) {
+    return VsResponseUtil.ok(bookingService.getBookingsForAdmin(pagination, bookingFilterDTO));
   }
 
-  @Operation(summary = "API create booking room")
+  @Operation(summary = "API create booking")
   @AuthorizationInfo(role = { RoleConstant.ADMIN, RoleConstant.USER })
   @PostMapping(value = UrlConstant.Booking.CREATE_BOOKING)
   public ResponseEntity<?> createBooking(@Valid @RequestBody BookingCreateDTO bookingCreateDTO,
-                                             @Parameter(name = "principal", hidden = true) @CurrentUserLogin UserPrincipal principal) {
+                                             @Parameter(name = "principal", hidden = true)
+                                             @CurrentUserLogin UserPrincipal principal) {
     return VsResponseUtil.ok(bookingService.createBooking(bookingCreateDTO, principal));
   }
 
