907a52fc6ddf5a1c2c03a2c3d577febaa0abc99e
huongbug <huongbug@gmail.com>
2023-05-18 22:42:04 +0700

    [week-8] change error message

--- src/main/java/com/bookinghotel/constant/ErrorMessage.java
+++ src/main/java/com/bookinghotel/constant/ErrorMessage.java
@@ -2,97 +2,97 @@ package com.bookinghotel.constant;
 
 public class ErrorMessage {
 
-  public static final String UNAUTHORIZED = "Sorry, you needs to provide authentication credentials to access this resource";
-  public static final String FORBIDDEN = "Sorry, you do not have the necessary permissions to access this resource";
-  public static final String FORBIDDEN_UD = "You do not have permission to edit or delete this resource";
-  public static final String ERR_EXCEPTION_GENERAL = "Something went wrong, please try again later";
+  public static final String UNAUTHORIZED = "Xin lỗi, bạn cần cung cấp thông tin xác thực để truy cập tài nguyên này!";
+  public static final String FORBIDDEN = "Xin lỗi, bạn không có quyền truy cập vào tài nguyên này!";
+  public static final String FORBIDDEN_UD = "Bạn không có quyền chỉnh sửa hoặc xóa tài nguyên này!";
+  public static final String ERR_EXCEPTION_GENERAL = "Có lỗi xảy ra, vui lòng thử lại sau!";
 
   //error validation dto
-  public static final String INVALID_SOME_THING_FIELD_IS_REQUIRED = "This field is required";
-  public static final String INVALID_SOME_THING_FIELD = "Invalid data";
-  public static final String INVALID_FORMAT_PASSWORD = "Unsatisfactory password";
-  public static final String NOT_BLANK_FIELD = "Can't be blank";
-  public static final String ERR_INVALID_FILE = "Invalid file format";
-  public static final String INVALID_DATE = "Invalid time";
-  public static final String INVALID_DATE_CHECK_IN = "Invalid check-in date";
-  public static final String INVALID_DATE_CHECK_OUT = "Invalid check-out date";
-  public static final String INVALID_FORMAT_SOME_THING_FIELD = "Invalid format";
+  public static final String INVALID_SOME_THING_FIELD_IS_REQUIRED = "Trường này là bắt buộc!";
+  public static final String INVALID_SOME_THING_FIELD = "Dữ liệu không hợp lệ!";
+  public static final String INVALID_FORMAT_PASSWORD = "Mật khẩu không đáp ứng yêu cầu!";
+  public static final String NOT_BLANK_FIELD = "Không thể để trống!";
+  public static final String ERR_INVALID_FILE = "Định dạng tệp không hợp lệ!";
+  public static final String INVALID_DATE = "Thời gian không hợp lệ!";
+  public static final String INVALID_DATE_CHECK_IN = "Ngày nhận phòng không hợp lệ!";
+  public static final String INVALID_DATE_CHECK_OUT = "Ngày trả phòng không hợp lệ!";
+  public static final String INVALID_FORMAT_SOME_THING_FIELD = "Định dạng không hợp lệ!";
 
   public static class Auth {
-    public static final String ERR_INCORRECT_AUTHENTICATION = "Username or password incorrect";
-    public static final String ERR_DUPLICATE_EMAIL = "Email is already taken";
-    public static final String ERR_DUPLICATE_PASSWORD = "The new password must be different from the old password";
-    public static final String ERR_ACCOUNT_NOT_ENABLED = "This account is not enabled";
-    public static final String ERR_ACCOUNT_LOCKED = "This account has been locked";
+    public static final String ERR_INCORRECT_AUTHENTICATION = "Tên đăng nhập hoặc mật khẩu không chính xác!";
+    public static final String ERR_DUPLICATE_EMAIL = "Email đã được sử dụng!";
+    public static final String ERR_DUPLICATE_PASSWORD = "Mật khẩu mới phải khác với mật khẩu cũ!";
+    public static final String ERR_ACCOUNT_NOT_ENABLED = "Tài khoản này chưa được kích hoạt!";
+    public static final String ERR_ACCOUNT_LOCKED = "Tài khoản này đã bị khóa!";
 
     //error token verification
-    public static final String INVALID_TOKEN = "Invalid token";
-    public static final String EXPIRED_TOKEN = "Token has expired";
-    public static final String INCORRECT_TOKEN = "Incorrect token";
+    public static final String INVALID_TOKEN = "Token không hợp lệ!";
+    public static final String EXPIRED_TOKEN = "Token đã hết hạn!";
+    public static final String INCORRECT_TOKEN = "Token không chính xác!";
   }
 
   public static class User {
-    public static final String ERR_NOT_FOUND_EMAIL_OR_PHONE = "User not found with this email or phone: %s";
-    public static final String ERR_NOT_FOUND_ID = "User not found with id: %s";
-    public static final String ERR_ACCOUNT_NOT_FOUND_BY_EMAIL = "User not found with email: %s";
-    public static final String ERR_CAN_NOT_UPDATE = "You cannot update other people's information";
-    public static final String ERR_CAN_NOT_PERMANENTLY_DELETED = "Non-locked users cannot be permanently deleted";
-    public static final String ERR_USER_NOT_ENABLED = "This account is not enabled";
-    public static final String ERR_USER_IS_LOCKED = "This account has been locked";
-    public static final String ERR_USER_IS_NOT_LOCKED = "This account has not been locked";
-    public static final String ERR_OLD_PASSWORD_IS_INCORRECT = "Old password is not incorrect";
-    public static final String ERR_NEW_PASSWORD_EQUAL_OLD_PASSWORD = "New password and old password cannot be the same";
+    public static final String ERR_NOT_FOUND_EMAIL_OR_PHONE = "Không tìm thấy người dùng với email hoặc số điện thoại: %s";
+    public static final String ERR_NOT_FOUND_ID = "Không tìm thấy người dùng với id: %s";
+    public static final String ERR_ACCOUNT_NOT_FOUND_BY_EMAIL = "Không tìm thấy người dùng với email: %s";
+    public static final String ERR_CAN_NOT_UPDATE = "Bạn không thể cập nhật thông tin người dùng khác!";
+    public static final String ERR_CAN_NOT_PERMANENTLY_DELETED = "Người dùng không khóa không thể bị xóa vĩnh viễn!";
+    public static final String ERR_USER_NOT_ENABLED = "Tài khoản này chưa được kích hoạt!";
+    public static final String ERR_USER_IS_LOCKED = "Tài khoản này đã bị khóa!";
+    public static final String ERR_USER_IS_NOT_LOCKED = "Tài khoản này chưa bị khóa!";
+    public static final String ERR_OLD_PASSWORD_IS_INCORRECT = "Mật khẩu cũ không chính xác!";
+    public static final String ERR_NEW_PASSWORD_EQUAL_OLD_PASSWORD = "Mật khẩu mới không được trùng với mật khẩu cũ!";
   }
 
   public static class Room {
-    public static final String ERR_NOT_FOUND_ID = "Room not found with id: %s";
-    public static final String ERR_NOT_FOUND_ID_IN_TRASH = "Room not found with id %s in the trash";
-    public static final String ERR_NO_PHOTO = "Room needs a photo to describe";
-    public static final String ERR_ROOM_UNAVAILABLE = "Sorry, this room is currently unavailable. Please choose another room";
-    public static final String ROOM_HAS_BEEN_DISCOUNTED = "Room %s has been discounted";
-    public static final String ROOM_NOT_FOR_SALE = "Room %s not for sale %s";
-    public static final String ROOM_NO_SALE = "Room %s not for sale";
+    public static final String ERR_NOT_FOUND_ID = "Không tìm thấy phòng với id: %s";
+    public static final String ERR_NOT_FOUND_ID_IN_TRASH = "Không tìm thấy phòng với id %s trong thùng rác!";
+    public static final String ERR_NO_PHOTO = "Phòng cần có một bức ảnh mô tả!";
+    public static final String ERR_ROOM_UNAVAILABLE = "Xin lỗi, phòng này hiện không khả dụng. Vui lòng chọn phòng khác!";
+    public static final String ROOM_HAS_BEEN_DISCOUNTED = "Phòng %s đã được giảm giá";
+    public static final String ROOM_NOT_FOR_SALE = "Phòng %s không có mã giảm giá %s";
+    public static final String ROOM_NO_SALE = "Phòng %s không có giảm giá";
   }
 
   public static class RoomRating {
-    public static final String ERR_NOT_FOUND_ID = "Room rating not found with id: %s";
-    public static final String ERR_CAN_NOT_UPDATE_OR_DELETED = "This room rating is not yours and cannot be updated or deleted";
+    public static final String ERR_NOT_FOUND_ID = "Không tìm thấy đánh giá với id: %s";
+    public static final String ERR_CAN_NOT_UPDATE_OR_DELETED = "Đánh giá phòng này không thuộc sở hữu của bạn và không thể cập nhật hoặc xóa!";
   }
 
   public static class Sale {
-    public static final String ERR_NOT_FOUND_ID = "Sale not found with id: %s";
-    public static final String ERR_NOT_FOUND_ID_IN_TRASH = "Sale not found with id %s in the trash";
-    public static final String ERR_CAN_NOT_PERMANENTLY_DELETED = "Sale must be in the trash to be permanently deleted";
+    public static final String ERR_NOT_FOUND_ID = "Không tìm thấy mã giảm giá với id: %s";
+    public static final String ERR_NOT_FOUND_ID_IN_TRASH = "Không tìm thấy mã giảm giá với id %s trong thùng rác";
+    public static final String ERR_CAN_NOT_PERMANENTLY_DELETED = "Mã giảm giá không có trong thùng rác để có thể bị xóa vĩnh viễn!";
   }
 
   public static class Service {
-    public static final String ERR_NOT_FOUND_ID = "Service not found with id: %s";
-    public static final String ERR_NOT_FOUND_ID_IN_TRASH = "Service not found with id: %s in the trash";
-    public static final String ERR_SERVICE_MUST_HAVE_THUMBNAIL = "Service must have a thumbnail";
+    public static final String ERR_NOT_FOUND_ID = "Không tìm thấy dịch vụ với id: %s";
+    public static final String ERR_NOT_FOUND_ID_IN_TRASH = "Không tìm thấy dịch vụ với id: %s trong thùng rác";
+    public static final String ERR_SERVICE_MUST_HAVE_THUMBNAIL = "Dịch vụ phải có hình ảnh mô tả";
   }
 
   public static class Product {
-    public static final String ERR_NOT_FOUND_ID = "Product not found with id: %s";
-    public static final String ERR_NOT_FOUND_ID_IN_TRASH = "Product not found with id: %s in the trash";
-    public static final String ERR_PRODUCT_MUST_HAVE_THUMBNAIL = "Product must have a thumbnail";
+    public static final String ERR_NOT_FOUND_ID = "Không tìm thấy sản phẩm với id: %s";
+    public static final String ERR_NOT_FOUND_ID_IN_TRASH = "Không tìm thấy sản phẩm với id: %s trong thùng rác";
+    public static final String ERR_PRODUCT_MUST_HAVE_THUMBNAIL = "Sản phẩm phải có hình ảnh mô tả!";
   }
 
   public static class Booking {
-    public static final String ERR_NOT_FOUND_ID = "Booking not found with id: %s";
-    public static final String ERR_NOT_CHECKIN = "This booking has not been checked in, cannot be checked out";
-    public static final String ERR_BOOKING_NOT_ROOM = "The room in the booking cannot be left empty";
-    public static final String ERR_CAN_NOT_CANCEL_BOOKING = "You can only cancel your booking one day in advance";
-    public static final String ERR_NO_PERMISSION_TO_CANCEL_BOOKING = "This booking is not yours and cannot be cancelled";
+    public static final String ERR_NOT_FOUND_ID = "Không tìm thấy booking với id: %s";
+    public static final String ERR_NOT_CHECKIN = "Đặt phòng này chưa checkin, không thể trả phòng";
+    public static final String ERR_BOOKING_NOT_ROOM = "Phòng trong booking không được bỏ trống";
+    public static final String ERR_CAN_NOT_CANCEL_BOOKING = "Bạn chỉ có thể hủy đặt phòng trước một ngày";
+    public static final String ERR_NO_PERMISSION_TO_CANCEL_BOOKING = "Booking này không thuộc sở hữu của bạn và không thể được hủy";
   }
 
   public static class Post {
-    public static final String ERR_NOT_FOUND_ID = "Post not found with id: %s";
-    public static final String ERR_NOT_FOUND_ID_IN_TRASH = "Post not found with id %s in the trash";
+    public static final String ERR_NOT_FOUND_ID = "Không tìm thấy bài viết với id: %s";
+    public static final String ERR_NOT_FOUND_ID_IN_TRASH = "Không tìm thấy bài viết với id %s trong thùng rác";
   }
 
   public static class Media {
-    public static final String ERR_NOT_FOUND_ID = "Media not found with id: %s";
-    public static final String ERR_NOT_FOUND_ID_IN_TRASH = "Media not found with id: %s in the trash";
+    public static final String ERR_NOT_FOUND_ID = "Không tìm thấy tệp với id: %s";
+    public static final String ERR_NOT_FOUND_ID_IN_TRASH = "Không tìm thấy tệp với id: %s trong thùng rác";
   }
 
 }
